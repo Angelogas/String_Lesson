74ec3a0fb413bce38163dc59b8df64eb7ce5d606
Gastran Njofang <angelogastran@gmail.com>
2021-09-22 23:33:25 -0500

    Practice question (Section 8) - Created fortune files
    I created the file containing the fortunes that will be used to solve the practice question.

--- SpringAnnotationsdem/src/com/luv2code/springdemo/DatabaseFortuneService.java
+++ SpringAnnotationsdem/src/com/luv2code/springdemo/DatabaseFortuneService.java
@@ -1,11 +1,16 @@
 package com.luv2code.springdemo;
 
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.stereotype.Component;
 
 @Component
 public class DatabaseFortuneService implements FortuneService{
+
     @Override
     public String getFortuneService() {
+        ClassPathXmlApplicationContext context =
+                new ClassPathXmlApplicationContext ("applicationContext.xml");
+//        context.getBean("");
         return null;
     }
 }
